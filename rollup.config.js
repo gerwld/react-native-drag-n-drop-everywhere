c50df8f5923926765ed69754677278dffff2b274
P Jaworski <pjaworski.dev@gmail.com>
2024-10-18 17:03:37 +0200

    build: added bugfix for default import Animated.View (commonJs)

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,20 @@ import typescript from 'rollup-plugin-typescript2';
 import json from '@rollup/plugin-json';
 import pkg from './package.json' with { type: "json" };
 
+function fixAnimatedDefault() {
+  return {
+    name: 'fix-animated-default',
+    renderChunk(code) {
+      // replaces 'Animated.View' with 'Animated.default.View' where necessary
+      return code.replace(/Animated\.View/g, 'Animated.View ? Animated.View : Animated.default.View');
+    }
+  };
+}
+
 export default [
   {
     input: 'src/index.tsx',
-    external: ['react', 'react-native'], // Treat as external dependencies
+    external: ['react', 'react-native', 'react-native-gesture-handler', 'react-native-reanimated'], // Treat as external dependencies
     output: [
       {
         file: pkg.main,  // CommonJS build
@@ -24,7 +34,8 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      json()
+      json(),
+      fixAnimatedDefault()
     ]
   }
 ];
