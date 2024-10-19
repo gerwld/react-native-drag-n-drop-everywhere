7905a75179ed27608084c1ddae068993e2d7215a
P Jaworski <pjaworski.dev@gmail.com>
2024-10-19 22:27:11 +0200

    added better sort handling, fixed renderGrip

--- src/DragItem.tsx
+++ src/DragItem.tsx
@@ -13,6 +13,28 @@ import Animated, {
   runOnJS,
 } from "react-native-reanimated";
 
+// handles callback for the new, sorted array
+const onCallbackData = (positions, callbackNewDataIds, prevArrayFromPositions) => {
+  "worklet"
+  const arrayFromPositions = Object.entries(positions)
+    .sort(([, indexA], [, indexB]) => {
+      const numIndexA = indexA as number; // Assert type as number
+      const numIndexB = indexB as number; // Assert type as number
+      return numIndexA - numIndexB;
+    }) // Sort by the index (value)
+    .map(([id]) => id); // Extract only the id (key)
+
+  const stringifiedArray = JSON.stringify(arrayFromPositions);
+
+  if (prevArrayFromPositions.current !== stringifiedArray) {
+    prevArrayFromPositions.current = stringifiedArray;
+
+    if (typeof callbackNewDataIds === "function") {
+      callbackNewDataIds(arrayFromPositions);
+    }
+  }
+}
+
 const clamp = (value, lowerBound, upperBound) => {
   "worklet";
   return Math.max(lowerBound, Math.min(value, upperBound));
@@ -46,7 +68,11 @@ const DragItem = (props) => {
     passVibration,
     itemBorderRadius,
     itemContainerStyle,
+    callbackNewDataIds,
     backgroundOnHold } = props;
+
+  const prevArrayFromPositions = React.useRef(null);
+  const currentRenderRef = React.useRef(1);  // TODO: better sort handler
 
   const pressed = useSharedValue(false);
   const offset = useSharedValue(0); // tracks the finger movement offset
@@ -67,7 +93,7 @@ const DragItem = (props) => {
   const isAndroid = Platform.OS === "android";
 
   const animatedStyles = useAnimatedStyle(() => {
-    
+
     let anim = {
       zIndex: pressed.value ? 1 : 0,
       top: top.value + offset.value,  // adjusts the top based on the offset
@@ -83,10 +109,14 @@ const DragItem = (props) => {
       shadowRadius: isAndroid ? 0 : 5,
       shadowOpacity: isAndroid ? 0 : withSpring(pressed.value ? 0.2 : 0)
     }
-    if(isAndroid)
+    if (isAndroid)
       return anim
     return animBetter
   });
+
+
+
+
 
   const pan = Gesture.Pan()
     .onBegin(() => {
@@ -95,6 +125,8 @@ const DragItem = (props) => {
       if (passVibration) passVibration();
       startY.value = top.value; // captures the initial Y position
     })
+
+
     .onChange((event) => {
       offset.value = event.translationY; // track how much the finger has moved
 
@@ -107,12 +139,20 @@ const DragItem = (props) => {
       );
 
       if (newPosition !== positions.value[item]) {
-        positions.value = objectMove(
+
+        const newMove = objectMove(
           positions.value,
           positions.value[item],
           newPosition
         );
-        if (passVibration) passVibration();
+        if (newMove && typeof newMove === "object") {
+          runOnJS(() => {
+            positions.value = newMove;
+          })();
+        }
+
+
+        if (typeof passVibration === "function") runOnJS(passVibration)();
       }
     })
     .onFinalize(() => {
@@ -120,6 +160,10 @@ const DragItem = (props) => {
       top.value = (positions.value[item] * (itemHeight + itemsGap)) + 10; // jump range on release
       top.value = withSpring(positions.value[item] * (itemHeight + itemsGap)); // snaps back to new position
       pressed.value = false;
+
+      // callback with ref when change was made
+      onCallbackData(positions.value, callbackNewDataIds, prevArrayFromPositions);
+
       runOnJS(setMoving)(false);
     });
 
@@ -127,7 +171,9 @@ const DragItem = (props) => {
     <Animated.View style={[styles.block, { borderRadius: itemBorderRadius, marginBottom: itemsGap }, itemContainerStyle, animatedStyles]}>
       <View style={styles.childContent}>{renderItem({ item })}</View>
       <GestureDetector gesture={pan}>
-        {renderGrip ? <View style={styles.grip}>{typeof renderGrip === "function" ? renderGrip() : renderGrip}</View> : <Text style={styles.useRG}>use the renderGrip prop :)</Text>}
+        {renderGrip
+          ? <View style={styles.grip}>{typeof renderGrip === "function" ? renderGrip() : renderGrip}</View>
+          : <Text style={styles.useRG}>use the renderGrip prop :)</Text>}
       </GestureDetector>
     </Animated.View>
   );
